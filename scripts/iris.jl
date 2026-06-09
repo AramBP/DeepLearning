bab2a017f6b1ab3382a46037e2d0cc869e7e17c5
Aram Brouwer <aramvanderpols05@gmail.com>
2026-06-09 09:43:19 +0200

    finished example for iris dataset

--- scripts/iris.jl
+++ scripts/iris.jl
@@ -4,6 +4,7 @@ Pkg.activate(".")
 
 using RDatasets
 using DeepLearning
+using Plots
 
 t(X) = copy(transpose(X))
 
@@ -13,8 +14,13 @@ y = convert(Vector{String}, iris.Species)
 
 X_train, y_train, X_test, y_test, classes = prepare_data(transpose(X), y; dims = 2)
 gd = GradientDescent(0.1)
-m, xs = train_perceptron(3, t(X_train), t(y_train), mse, mse_grad, sigmoid, deriv_sigmoid, gd; max_it = 1000)
-predictions = onecold(t(predict(m, t(X_test))), classes)
-targets = onecold(y_test, classes)
-cf = confmat(predictions, targets, classes)
-@show cf
+m, xs = train_perceptron(3, t(X_train), t(y_train), mse, mse_grad, sigmoid, deriv_sigmoid, gd; max_it = 200)
+cf = confmat(onecold(t(predict(m, t(X_test))), classes), onecold(y_test, classes), classes)
+
+loss_vals = []
+for x in xs
+    m.params = x
+    append!(loss_vals, mse(t(y_test), predict(m, t(X_test))))
+end
+
+plot(1:length(loss_vals),loss_vals)
